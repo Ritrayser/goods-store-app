6c80adbec131c105311cb0b2eaa026c17350290a
Egor Lykov <egor.lykov2020@gmail.com>
2026-04-24 19:47:21 +0300

    Иницилизация frontend

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Inertia\Inertia;
+
 class WebsiteController extends Controller
 {
     public function home()
     {
-        return 'Привет';
+        return Inertia::render('app');
     }
 }
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    @vite('resources/js/app.js')
-    @inertiaHead
-</head>
-<body class="bg-gray-50 antialiased">
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+  </head>
+  <body>
     @inertia
-</body>
+  </body>
 </html>
